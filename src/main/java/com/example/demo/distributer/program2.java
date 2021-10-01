2aef0252e76aa3d669443b74a3d9cfdefd418355
Inho Jung <inho.jeoung@gmail.com>
2021-10-01 17:31:48 +0900

    Create program2.java

---
+++ src/main/java/com/example/demo/distributer/program2.java
@@ -0,0 +1,14 @@
+package com.example.demo.distributer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class program2 {
+
+	@GetMapping("/go")
+	public @ResponseBody String path() {
+		return "I want a go home";
+	}
+}
