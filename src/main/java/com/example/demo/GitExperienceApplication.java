c11d300a4cb06bdd557d394b8d2aba4eddd72bd9
Inho Jung <inho.jeoung@gmail.com>
2021-10-01 13:28:04 +0900

    initial commit

---
+++ src/main/java/com/example/demo/GitExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExperienceApplication.class, args);
+	}
+
+}
