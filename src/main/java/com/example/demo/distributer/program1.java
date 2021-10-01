c09847cc7283bb8cb2e93c885d14a7fea68de3bb
Inho Jung <inho.jeoung@gmail.com>
2021-10-01 13:54:16 +0900

    pro1

---
+++ src/main/java/com/example/demo/distributer/program1.java
@@ -0,0 +1,15 @@
+package com.example.demo.distributer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class program1 {
+	
+	@GetMapping("/helloWorld")
+	public @ResponseBody String sampleFunction1() {
+		return "helloWorld";
+	}
+
+}
